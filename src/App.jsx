fce2404617494f6a61343105f2ff0f545f2a3fe6
Galen Hutchison <galen.hutchison@beam.dental>
2023-02-16 17:14:57 -0600

    search returns values and keys

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,49 @@ function App() {
     return res;
   }
 
+  const dummyData = [
+    {
+      "XXXIV.": {
+        columnName: "Some Greek Gods",
+        0: "Pan",
+        1: "Zeus, Iacchus",
+        2: "Athena, Uranus [[Hermes]]",
+        3: "Cybele, Demeter, Rhea, Heré, [[Psyché, Kronos]]",
+        4: "Poseidon [[Zeus]]",
+        5: "Ares, Hades",
+        6: "Iacchus, Apollo, Adonis [[Dionysus, Bacchus]]",
+        7: "Aphrodité, Niké",
+        8: "Hermes",
+        9: "Zeus (as D), Diana of Epheus (as phallic stone [[and =]]) [[Eros]] ",
+        10: "Persephone, [Adonis], Psyché",
+        11: "Zeus",
+        12: "Hermes ",
+        13: "Artemis, Hekaté ",
+        14: "Aphrodité ",
+        15: "Athena",
+        16: "[Heré]",
+        17: "Castor and Pollux, Apollo the Diviner [[Eros]]",
+        18: "Apollo the Charioteer",
+        19: "Demeter [borne by lions]",
+        20: "[Attis]",
+        21: "Zeus",
+        22: "Themis, Minos, Aeacus and Rhadamanthus",
+        23: "Poseidon",
+        24: "Ares [[Apollo the Pythean, Thanatos]]",
+        25: "Apollo, Artemis (hunters)",
+        26: "Pan, Priapus [Erect Hermes and Bacchus]",
+        27: "Ares, [[Athena]]",
+        28: "[Athena] Ganymede",
+        29: "Poseidon [[Hermes Psychopompos]]",
+        30: "Helios, Apollo",
+        31: "Hades",
+        32: "[Athena]",
+        "32 bis": "[Demeter] [[Gaia]]",
+        "31 bis": "Iacchus",
+      },
+    },
+  ];
+
   //Search Functions
   //
   function toTitleCase(str) {
@@ -27,7 +70,7 @@ function App() {
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
   }
-
+  //TODO: needs to remove special characters
   function checkIfTargetExists(target, data) {
     return JSON.stringify(data).indexOf(toTitleCase(target)) > -1;
   }
@@ -52,33 +95,46 @@ function App() {
     return null;
   }
 
-  //TODO: fix this function
-  function customFilter(object, col, result) {
-    console.log(object);
-    console.log(result);
-    if (object.hasOwnProperty("columnName")) result.push(object[col]);
-
-    for (var i = 0; i < Object.keys(object).length; i++) {
-      if (typeof object[Object.keys(object)[i]] == "object") {
-        customFilter(object[Object.keys(object)[i]], result);
-      }
-    }
-  }
-
   function handleSearch(e) {
-    console.log(target);
-    const col = filterByValue(data, toTitleCase(target));
-    const res = [];
-    console.log(col);
-    customFilter(data, col, res);
-    console.log(res);
-    // if (checkIfTargetExists(target, data)) {
-    //   console.log("found-- performing lookup");
-    // } else {
-    //   console.log("target not found");
-    // }
-    // const res = data[2].find((elem) => elem.includes(target));
-    // console.log(res);
+    console.log(target); //TODO: make target auto capitalize
+
+    //TODO: optimize this algorithm
+    //for each element in data do
+    //check if target exists in element
+    //if it does, capture the column name then perform the following:
+
+    // check if target exists in blob
+    checkIfTargetExists(target, dummyData)
+      ? console.log("found")
+      : console.log("not found");
+
+    // if it does, split blob into array of strings
+    const splitBlob = JSON.stringify(dummyData).split(":");
+    console.log(splitBlob);
+
+    // filter into array of indexes where target exists
+    const hits = splitBlob
+      .map((elem, index) => {
+        return elem.indexOf(target) > -1 ? index : null;
+      })
+      .filter((n) => n);
+
+    // map indexes to two arrays: column keys, and string values
+    const colKeys = hits.map((i) => {
+      var targ = splitBlob[i - 1];
+      return targ.split(",").slice(-1)[0]; //last element is key as it appears before the ':'
+    });
+
+    const colVals = hits.map((i) => {
+      //pops the tailing key from the string
+      var targ = splitBlob[i].split(",");
+      targ.pop();
+      return targ.join(",");
+    });
+
+    colKeys.forEach((elem, index) => {
+      console.log(elem, colVals[index]);
+    });
   }
 
   //END Search Functions
@@ -86,7 +142,7 @@ function App() {
   function onTargetUpdate(e) {
     setTarget(e.target.value);
   }
-
+  //TODO: return a list of results while typing
   return (
     <div className="App">
       <SearchBar
